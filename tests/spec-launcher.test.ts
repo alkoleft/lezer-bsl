2e455e035d1bb41b36ea00d0bc019be8cb84f056
alkoleft <alkoleft@gmail.com>
2026-01-28 02:34:56 +0300

    feat: работа над грамматикой

---
+++ tests/spec-launcher.test.ts
@@ -0,0 +1,24 @@
+import { describe, test } from "vitest"
+import { fileTests } from "@lezer/generator/dist/test"
+import { parser } from "../src/parser/bslParser"
+
+import * as fs from "node:fs"
+import * as path from "node:path"
+import { fileURLToPath } from "node:url"
+
+const caseDir = path.resolve(path.dirname(fileURLToPath(import.meta.url)), "spec")
+
+// Включаем диалект comments для парсера
+const parserWithComments = parser.configure({ dialect: "comments", strict: true })
+
+for (const file of fs.readdirSync(caseDir)) {
+  if (!/\.txt$/.test(file)) continue
+
+  const content = fs.readFileSync(path.join(caseDir, file), "utf8")
+
+  describe(file, () => {
+    for (const { name, run } of fileTests(content, file)) {
+      test(name, () => run(parserWithComments))
+    }
+  })
+}
