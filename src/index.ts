49e9c5ddb54a1ac44ee61ff707d699bd31f18f33
alkoleft <alkoleft@gmail.com>
2026-02-01 23:09:05 +0300

    build: переход на pnpm

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { parser } from './bslParser.js'
+export { LezerBslParser } from './parser/LezerBslParser.js'
+export * from './bslParser.terms.js'
+export { bslHighlighting } from './highlight.js'
